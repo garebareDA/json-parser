2f7c7d65e0828df8f0b0287867a192c820a32373
garebareDA <garedaiki@outlook.jp>
2021-08-24 07:03:07 +0900

    add:lexBool

--- lib/lexer.go
+++ lib/lexer.go
@@ -2,48 +2,82 @@ package lexer
 
 import (
 	"errors"
+	"log"
 )
 
 const jsonQuote = '"'
 
 type jsonString string
+type jsonBool bool
+
+type lexer struct {
+	input []rune
+	tokens []interface{}
+}
+
+func newLexer(input string) lexer {
+	return lexer {
+		input: []rune(input),
+	}
+}
 
 func Lex(input string) ([]interface{}, error) {
-	str := []rune(input)
-	var tokens []interface{}
-
-	for len(str) > 0 {
-		jsonStrings, s, err := lexString(str)
-		str = s;
+	lex := newLexer(input)
+	for len(lex.input) > 0 {
+		jsonStrings, err := lex.lexString()
 		if err != nil {
 			return nil, err
 		}
 		if len(jsonStrings) > 0 {
-			tokens = append(tokens, jsonStrings)
+			lex.tokens = append(lex.tokens, jsonStrings)
 			continue
 		}
 
-		return nil, errors.New("unexpected character " + string(str))
+		jsonBool, isOk := lex.lexBool()
+		if isOk {
+			lex.tokens = append(lex.tokens, jsonBool)
+			continue
+		}
+
+		return nil, errors.New("unexpected character " + string(lex.input[0]))
 	}
 
-	return tokens, nil
+	return lex.tokens, nil
 }
 
-func lexString(str []rune) (jsonString, []rune, error) {
-	if jsonQuote == str[0] {
-		str = str[1:]
+func (l *lexer) lexString () (jsonString, error) {
+	if jsonQuote == l.input[0] {
+		l.input = l.input[1:]
 	} else {
-		return "", str, nil
+		return "", nil
 	}
 
 	var strings []rune
-	for i, char := range str {
+	for i, char := range l.input {
 		if char == jsonQuote {
-			str = str[i+1:]
-			return jsonString(strings), str, nil
+			l.input = l.input[i+1:]
+			return jsonString(strings),  nil
 		}
 		strings = append(strings, char)
 	}
 
-	return "", str, errors.New("expected end-of-string quote")
+	return "", errors.New("expected end-of-string quote")
+}
+
+func (l *lexer) lexBool() (jsonBool, bool) {
+	trueLen := len("true")
+	falseLen := len("false")
+
+	if string(l.input[0:trueLen]) == "true" {
+		l.input = l.input[trueLen:]
+		log.Println(l.input)
+		return jsonBool(true), true
+	}
+
+	if string(l.input[0:falseLen]) == "false" {
+		l.input = l.input[falseLen:]
+		return jsonBool(false), true
+	}
+
+	return false, false
 }
--- lib/lexer_test.go
+++ lib/lexer_test.go
@@ -7,10 +7,26 @@ import (
 )
 
 func TestJsonString(t *testing.T) {
-	tokens, err := Lex("\"test\"");
+	tokens, err := Lex("\"test\"")
 	if err != nil {
 		t.Fatalf("string error: %v", err)
 	}
 
 	assert.Equal(t, jsonString("test"), tokens[0])
+}
+
+func TestJsonBoolTrue(t *testing.T) {
+	tokens, err := Lex("true")
+	if err != nil {
+		t.Fatalf("bool error: %v", err)
+	}
+	assert.Equal(t, jsonBool(true), tokens[0])
+}
+
+func TestJsonBoolFalse(t *testing.T) {
+	tokens, err := Lex("false")
+	if err != nil {
+		t.Fatalf("bool error: %v", err)
+	}
+	assert.Equal(t, jsonBool(false), tokens[0])
 }
