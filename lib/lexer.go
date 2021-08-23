14687ed1a67f2ad162c0c83380c28c03f4b71a26
garebareDA <garedaiki@outlook.jp>
2021-08-23 20:35:27 +0900

    wip

--- lib/lexer.go
+++ lib/lexer.go
@@ -1,2 +1,34 @@
 package lexer
 
+const jsonQuote = '/'
+
+type jsonString string
+
+func Lex(input string) ([]interface{}, error) {
+	str := []rune(input)
+	var tokens []interface{}
+
+	for len(str) > 0 {
+		var jsonString string
+		var err error
+
+	}
+
+	return tokens, nil
+}
+
+func lexString(str []rune) jsonString {
+	if jsonQuote == str[0] {
+		str = str[1:]
+	} else {
+		return ""
+	}
+
+	for _, char := range str {
+		if char == jsonQuote {
+
+		}
+	}
+
+	return ""
+}
