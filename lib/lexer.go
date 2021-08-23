69b191771196728f78003fb220fe2ba53d01ae1a
garebareDA <garedaiki@outlook.jp>
2021-08-23 23:28:48 +0900

    add:jsonstirng

--- lib/lexer.go
+++ lib/lexer.go
@@ -1,5 +1,9 @@
 package lexer
 
+import (
+	"fmt"
+)
+
 const jsonQuote = '/'
 
 type jsonString string
@@ -9,26 +13,35 @@ func Lex(input string) ([]interface{}, error) {
 	var tokens []interface{}
 
 	for len(str) > 0 {
-		var jsonString string
-		var err error
-
+		jsonStrings, ret, err := lexString(str)
+		str = ret;
+		if err != nil {
+			return nil, err
+		}
+		if len(jsonStrings) > 0 {
+			tokens = append(tokens, jsonStrings)
+			continue
+		}
 	}
 
 	return tokens, nil
 }
 
-func lexString(str []rune) jsonString {
+func lexString(str []rune) (jsonString, []rune, error) {
 	if jsonQuote == str[0] {
 		str = str[1:]
 	} else {
-		return ""
+		return "", str, nil
 	}
 
-	for _, char := range str {
+	var strings []rune
+	for i, char := range str {
 		if char == jsonQuote {
-
+			str = str[i:]
+			return jsonString(strings), str, nil
 		}
+		strings = append(strings, char)
 	}
 
-	return ""
+	return "", str, fmt.Errorf("Expected end-of-string quote")
 }
